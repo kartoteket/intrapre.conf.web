4b2d58d56a076da0fd8afd94d7532da2a7dc6a7e
Svale Fossåskaret <svale@3by5.no>
2016-03-26 09:26:09 +0100

    Adds section titles with js (deb bliss.js) for last word styling

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,16 @@
-// This is where it all goes :)
+/**
+ * style section headings
+ */
+$$('.section-title h4').forEach(function(h4){
+
+    var text = h4.textContent.split(/[\s-]/)
+      , lastWord = text.pop();
+    h4.textContent = text.join(' ');
+    h4._.set({
+        contents: {
+            tag: "span",
+            class: 'color-primary',
+            textContent: ' ' + lastWord
+        }
+    });
+});
